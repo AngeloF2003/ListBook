953c7ed4ad845054d969cb403408ce7ad06e2551
Angelo Santana <angelo.santana@materiagris.pe>
2024-08-12 18:25:39 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksModule } from './books/books.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BooksModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
